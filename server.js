e26131f836e9e8cb1c2c7c237350a4289d3cf3bd
sadside <75984650+sadside@users.noreply.github.com>
2022-06-12 15:50:04 +0500

    s

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = 8080;
 
 const app = express();
 app.use(express.static(__dirname));
-app.use(express.static(path.resolve(_ - dirname, "build")));
+app.use(express.static(path.resolve(__dirname, "build")));
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
